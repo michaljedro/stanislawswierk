e30078c903802ba5bffdba8793763547e03cbe2e
Michał Jędro <michal.jedro@wp.pl>
2022-01-03 17:17:43 +0100

    change concept of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,23 +8,25 @@ function Navbar() {
     const [isMobile, setIsMobile] = useState(true);
 
     return (
-        <div className='navbar'>
-                <h3 className='logo'>Stanisław Świerk</h3>
-                <ul className={isMobile? 'nav-links-mobile': 'nav-links'}
-                >
-                {SidebarData.map((item) => {
-                    const { id, title, path } = item;
-                        return (
-                                <Link to={path} key={id}className='list-item'>
-                                    <li>{title}</li>
-                                </Link>
-                        );
-                    })}
-                    </ul>
-                <button className='mobile-menu-icon' onClick={()=>setIsMobile(!isMobile)}>
-                    {isMobile ?<FaTimes className='icon'/>: <FaBars className='icon'/>}
-                </button>
-            </div>
+        <div className='container'>
+            <div className='navbar'>
+                    <h3 className='logo'>Stanisław Świerk</h3>
+                    <ul className={isMobile? 'nav-links-mobile': 'nav-links'}
+                    >
+                    {SidebarData.map((item) => {
+                        const { id, title, path } = item;
+                            return (
+                                    <Link to={path} key={id}className='list-item'>
+                                        <li>{title}</li>
+                                    </Link>
+                            );
+                        })}
+                        </ul>
+                    <button className='mobile-menu-icon' onClick={()=>setIsMobile(!isMobile)}>
+                        {isMobile ?<FaTimes className='icon'/>: <FaBars className='icon'/>}
+                    </button>
+                </div>
+        </div>
     )  
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
-const Division = styled.div`
-    background-color:black;
-    border: 5px solid green;
-`
+import './Home.css';
+
 function Home() {
     return (
-        <Division>
+        <div className='division'>
             HomePage
-        </Division>
+        </div>
     )
 }
 
