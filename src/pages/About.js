bf78255817f6a95b9b207e81d8f9259869c18492
Michał Jędro <michal.jedro@wp.pl>
2022-01-05 20:44:12 +0100

    end of styling about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,34 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Container } from '../styles/Container.styled'
+import Photo from '../assets/2.png';
 const Division = styled.div`
     display:flex;
-    border: 5px solid green;
+    height:70vh;
+    // border: 2px solid yellow;
+`
+const TextDiv = styled.div`
+    flex:1;
+    display:flex;
+    align-items:center;
+`
+const DivisionFlex = styled.div`
+    display:flex;
     height:100%;
 `
 const PhotoDiv = styled.div`
     background-color: white;
-    flex-grow:1;
-`
-const TextDiv = styled.div`
-    background-color:blue;
-    flex-grow:1;
-`
-const DivisionFlex = styled.div`
+    flex:1;
     display:flex;
-    border:1px solid pink;
-    height:100%
+    align-items: center;
+    justify-content:center;
 `
+const PhotoImg = styled.img`
+    height:70%;
+`
+
 function About() {
     return (
             <Division>
                 <Container>
                     <DivisionFlex>
                         <TextDiv>
-                            Text
+                            <p>Praesent pretium lectus nunc, quis euismod arcu venenatis at. Integer non nibh sit amet est feugiat dictum at a risus. Donec eleifend, arcu tempus volutpat elementum, leo leo sodales nisi, a suscipit nulla urna egestas ligula. Nulla venenatis lectus nunc, eget pharetra nibh porta non. In hac habitasse platea dictumst. Ut ullamcorper pulvinar ornare. Aliquam erat volutpat. Pellentesque egestas lobortis imperdiet. Nam imperdiet pellentesque ornare. Praesent in tempus odio.</p>
                         </TextDiv>
                         <PhotoDiv>
-                            Photo
+                            <PhotoImg src={Photo}/>
                         </PhotoDiv>
                     </DivisionFlex>
                 </Container>
